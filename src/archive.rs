252b8c4cba5c29f8980f6663b5efd7fb28f95632
mattsse <matthias.seitz@tum.de>
2020-03-15 22:08:44 +0100

    add archive creation support

--- src/archive.rs
+++ src/archive.rs
@@ -1,5 +1,13 @@
+use flate2::write::GzEncoder;
+use flate2::Compression;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{Seek, Write};
+use std::iter::Iterator;
 use std::path::{Path, PathBuf};
 use std::{fs, io};
+use walkdir::{DirEntry, WalkDir};
+use zip::write::FileOptions;
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub enum ArchiveMode {
@@ -12,16 +20,28 @@ impl ArchiveMode {
     /// Create either a tar or zip file of the provided source path
     pub fn make_archive(
         &self,
-        _src: impl AsRef<Path>,
-        _to: impl AsRef<Path>,
+        src: impl AsRef<Path>,
+        to: impl AsRef<Path>,
     ) -> anyhow::Result<Option<PathBuf>> {
-        // TODO implement
         match self {
-            ArchiveMode::Tar => {}
-            ArchiveMode::Zip => {}
-            ArchiveMode::Skip => {}
+            ArchiveMode::Tar => {
+                let to = PathBuf::from(format!("{}.tar.gz", to.as_ref().display()));
+                let file = File::create(&to)?;
+                make_tarball(src, file)?;
+                Ok(Some(to))
+            }
+            ArchiveMode::Zip => {
+                let src = src.as_ref();
+                let to = PathBuf::from(format!("{}.zip", to.as_ref().display()));
+                let file = File::create(&to)?;
+                let walkdir = WalkDir::new(src);
+                let it = walkdir.into_iter();
+
+                zip_dir(&mut it.filter_map(|e| e.ok()), src, file)?;
+                Ok(Some(to))
+            }
+            ArchiveMode::Skip => Ok(None),
         }
-        Ok(None)
     }
 }
 
@@ -64,5 +84,48 @@ pub(crate) fn unzip(file: impl AsRef<Path>) -> anyhow::Result<()> {
         }
     }
 
+    Ok(())
+}
+
+fn zip_dir<T>(
+    it: &mut impl Iterator<Item = DirEntry>,
+    prefix: impl AsRef<Path>,
+    writer: T,
+) -> anyhow::Result<()>
+where
+    T: Write + Seek,
+{
+    let prefix = prefix.as_ref();
+    let mut zip = zip::ZipWriter::new(writer);
+    let options = FileOptions::default().unix_permissions(0o755);
+
+    let mut buffer = Vec::new();
+    for entry in it {
+        let path = entry.path();
+        let name = path.strip_prefix(prefix)?;
+
+        // Write file or directory explicitly
+        // Some unzip tools unzip files with directory paths correctly, some do not!
+        if path.is_file() {
+            zip.start_file_from_path(name, options)?;
+            let mut f = File::open(path)?;
+
+            f.read_to_end(&mut buffer)?;
+            zip.write_all(&*buffer)?;
+            buffer.clear();
+        } else if name.as_os_str().len() != 0 {
+            // Only if not root! Avoids path spec / warning
+            // and mapname conversion failed error on unzip
+            zip.add_directory_from_path(name, options)?;
+        }
+    }
+    zip.finish()?;
+    Ok(())
+}
+
+fn make_tarball<T: Write>(src: impl AsRef<Path>, writer: T) -> anyhow::Result<()> {
+    let enc = GzEncoder::new(writer, Compression::default());
+    let mut tar = tar::Builder::new(enc);
+    tar.append_dir_all(".", src)?;
     Ok(())
 }
